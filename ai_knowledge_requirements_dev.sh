01c31fdb892545da63446a6a242dbb353c1b9e6d
Idea Bosque <ideabosque@gmail.com>
2025-04-30 12:58:51 -0700

    Add ai knowledge lambda.

---
+++ ai_knowledge_requirements_dev.sh
@@ -0,0 +1,4 @@
+### Private modules.
+echo "y" | pip uninstall ai_knowledge_engine && pip install git+ssh://git@github.com/ideabosque/ai_knowledge_engine.git@main#egg=ai_knowledge_engine
+
+python3.11 cloudformation_stack.py .env silvaengine-microcore-ai-knowledge
