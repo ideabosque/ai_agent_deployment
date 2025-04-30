3833bc8cf323744970fed764f6fbf0e81a128cf8
Idea Bosque <ideabosque@gmail.com>
2025-04-30 12:54:24 -0700

    Add ai knowledge lambda.

--- ai_agent_requirements_dev.sh
+++ ai_agent_requirements_dev.sh
@@ -1,6 +1,4 @@
-echo "y" | pip uninstall openai && pip install openai
 echo "y" | pip uninstall toml && pip install toml
-echo "y" | pip uninstall tiktoken && pip install tiktoken
 echo "y" | pip uninstall ShopifyAPI && pip install ShopifyAPI
 echo "y" | pip uninstall shopify_connector && pip install git+https://github.com/ideabosque/shopify_connector.git@main#egg=shopify_connector
 echo "y" | pip uninstall silvaengine_resource && pip install git+https://github.com/ideabosque/silvaengine_resouces.git@main#egg=silvaengine_resource
@@ -14,6 +12,6 @@ echo "y" | pip uninstall gemini_agent_handler && pip install git+https://github.
 echo "y" | pip uninstall anthropic_agent_handler && pip install git+https://github.com/ideabosque/anthropic_agent_handler.git@main#egg=anthropic_agent_handler
 
 ### Private modules.
-echo "y" | pip uninstall ai_knowledge_engine && pip install git+ssh://git@github.com/ideabosque/ai_knowledge_engine.git@main#egg=ai_knowledge_engine
+echo "y" | pip uninstall ai_knowledge_engine && pip install git+ssh://git@github.com/ideabosque/ai_knowledge_engine.git@dev#egg=ai_knowledge_engine
 
 python3.11 cloudformation_stack.py .env silvaengine-microcore-ai-agent
