851b6b3c1d339e4427835971d69089c9d1926f43
Barry Liu <barry.y.liu@hotmail.com>
2025-05-15 09:17:45 +0800

    [U] Update dependencies

--- ai_knowledge_requirements_dev.sh
+++ ai_knowledge_requirements_dev.sh
@@ -1,4 +1,5 @@
 ### Private modules.
 echo "y" | pip uninstall ai_knowledge_engine && pip install git+ssh://git@github.com/ideabosque/ai_knowledge_engine.git@dev#egg=ai_knowledge_engine
+echo "y" | pip uninstall shopify_connector && pip install git+ssh://git@github.com/ideabosque/shopify_connector.git@main#egg=shopify_connector
 
 python3.11 cloudformation_stack.py .env silvaengine-microcore-ai-knowledge
