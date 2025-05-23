392f649ddc93e8ba062b28f663ca9c3bf3a7b4ba
Idea Bosque <ideabosque@gmail.com>
2025-05-22 18:43:21 -0700

    Update the deployment scripts.

--- slack_requirements.sh
+++ slack_requirements.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
+# Check for environment file argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <env_file_path>"
+  exit 1
+fi
+
+ENV_FILE="$1"
+
 echo "y" | pip uninstall slack_bolt && pip install slack_bolt
 echo "y" | pip uninstall silvaengine_resource && pip install git+https://github.com/ideabosque/silvaengine_resouces.git@main#egg=silvaengine_resource
 echo "y" | pip uninstall slack_bot_engine && pip install git+ssh://git@github.com/ideabosque/slack_bot_engine.git@main#egg=slack_bot_engine
 
-python3.11 cloudformation_stack.py .prod.env silvaengine-microcore-slack
+python3.11 cloudformation_stack.py "$ENV_FILE" silvaengine-microcore-slack
