7c957bc21e1c9973cf7893427500fad867348dd6
Idea Bosque <ideabosque@gmail.com>
2025-05-26 18:31:41 -0700

    Update ai_knowledge_requirements.sh.

--- ai_knowledge_requirements.sh
+++ ai_knowledge_requirements.sh
@@ -7,8 +7,9 @@ if [ -z "$1" ]; then
 fi
 
 ENV_FILE="$1"
+BRANCH="${2:-main}"
 
 ### Private modules.
-echo "y" | pip uninstall ai_knowledge_engine && pip install git+ssh://git@github.com/ideabosque/ai_knowledge_engine.git@main#egg=ai_knowledge_engine
+echo "y" | pip uninstall ai_knowledge_engine && pip install git+ssh://git@github.com/ideabosque/ai_knowledge_engine.git@$BRANCH#egg=ai_knowledge_engine
 
 python3.11 cloudformation_stack.py "$ENV_FILE" silvaengine-microcore-ai-knowledge
