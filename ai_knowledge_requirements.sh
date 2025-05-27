51ef4ad619d51b4cae4ab5d738a8c98be3cbc2dd
Idea Bosque <ideabosque@gmail.com>
2025-05-26 22:43:10 -0700

    Update deploy script.

--- ai_knowledge_requirements.sh
+++ ai_knowledge_requirements.sh
@@ -9,8 +9,10 @@ fi
 ENV_FILE="$1"
 BRANCH="${2:-main}"
 
-### Private modules.
 echo "y" | pip uninstall ShopifyAPI && pip install ShopifyAPI
+echo "y" | pip uninstall redis_stack_connector && pip install git+https://github.com/ideabosque/redis_stack_connector.git@main#egg=redis_stack_connector
+echo "y" | pip uninstall neo4j_graph_connector && pip install git+https://github.com/ideabosque/neo4j_graph_connector.git@main#egg=neo4j_graph_connector
+### Private modules.
 echo "y" | pip uninstall ai_knowledge_engine && pip install git+ssh://git@github.com/ideabosque/ai_knowledge_engine.git@$BRANCH#egg=ai_knowledge_engine
 
 python3.11 cloudformation_stack.py "$ENV_FILE" silvaengine-microcore-ai-knowledge
