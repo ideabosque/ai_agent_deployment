75e0afbb3ffe2767a0dd3632b496ab324ed47c10
Idea Bosque <ideabosque@gmail.com>
2025-06-03 19:20:51 -0700

    Update slack_requirements.sh.

--- slack_requirements.sh
+++ slack_requirements.sh
@@ -7,9 +7,10 @@ if [ -z "$1" ]; then
 fi
 
 ENV_FILE="$1"
+BRANCH="${2:-main}"
 
 echo "y" | pip uninstall slack_bolt && pip install slack_bolt
 echo "y" | pip uninstall silvaengine_resource && pip install git+https://github.com/ideabosque/silvaengine_resouces.git@main#egg=silvaengine_resource
-echo "y" | pip uninstall slack_bot_engine && pip install git+ssh://git@github.com/ideabosque/slack_bot_engine.git@main#egg=slack_bot_engine
+echo "y" | pip uninstall slack_bot_engine && pip install git+ssh://git@github.com/ideabosque/slack_bot_engine.git@$BRANCH#egg=slack_bot_engine
 
 python3.11 cloudformation_stack.py "$ENV_FILE" silvaengine-microcore-slack
