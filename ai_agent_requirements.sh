6b26d4c476d419e017ab25da5ee781c6ba70f526
Idea Bosque <ideabosque@gmail.com>
2025-08-11 09:15:03 -0700

    Add shopify_app_engine.

--- ai_agent_requirements.sh
+++ ai_agent_requirements.sh
@@ -24,5 +24,6 @@ echo "y" | pip uninstall gemini_agent_handler && pip install git+https://github.
 echo "y" | pip uninstall anthropic_agent_handler && pip install git+https://github.com/ideabosque/anthropic_agent_handler.git@$BRANCH#egg=anthropic_agent_handler
 echo "y" | pip uninstall mcp_http_client && pip install git+https://github.com/ideabosque/mcp_http_client.git@main#egg=mcp_http_client
 echo "y" | pip uninstall app_core_engine && pip install git+https://github.com/ideabosque/app_core_engine.git@main#egg=app_core_engine
+echo "y" | pip uninstall shopify_app_engine && pip install git+https://github.com/ideabosque/shopify_app_engine.git@main#egg=shopify_app_engine
 
 python3.11 cloudformation_stack.py "$ENV_FILE" silvaengine-microcore-ai-agent
