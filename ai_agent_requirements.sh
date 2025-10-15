5263de16ceecb5fdd82eac7fbb8461b69c815ea9
Idea Bosque <ideabosque@gmail.com>
2025-10-15 16:06:44 -0700

    Update deployment script.

--- ai_agent_requirements.sh
+++ ai_agent_requirements.sh
@@ -15,7 +15,6 @@ echo "y" | pip uninstall openai && pip install openai
 echo "y" | pip uninstall shopify_connector && pip install git+https://github.com/ideabosque/shopify_connector.git@main#egg=shopify_connector
 echo "y" | pip uninstall silvaengine_resource && pip install git+https://github.com/ideabosque/silvaengine_resouces.git@main#egg=silvaengine_resource
 echo "y" | pip uninstall ai_agent_core_engine && pip install git+https://github.com/ideabosque/ai_agent_core_engine.git@$BRANCH#egg=ai_agent_core_engine
-echo "y" | pip uninstall ai_agent_funct_base && pip install git+https://github.com/ideabosque/ai_agent_funct_base.git@$BRANCH#egg=ai_agent_funct_base
 echo "y" | pip uninstall ai_marketing_engine && pip install git+https://github.com/ideabosque/ai_marketing_engine.git@$BRANCH#egg=ai_marketing_engine
 echo "y" | pip uninstall ai_coordination_engine && pip install git+https://github.com/ideabosque/ai_coordination_engine.git@$BRANCH#egg=ai_coordination_engine
 echo "y" | pip uninstall ai_agent_handler && pip install git+https://github.com/ideabosque/ai_agent_handler.git@$BRANCH#egg=ai_agent_handler
@@ -23,8 +22,8 @@ echo "y" | pip uninstall openai_agent_handler && pip install git+https://github.
 echo "y" | pip uninstall gemini_agent_handler && pip install git+https://github.com/ideabosque/gemini_agent_handler.git@$BRANCH#egg=gemini_agent_handler
 echo "y" | pip uninstall anthropic_agent_handler && pip install git+https://github.com/ideabosque/anthropic_agent_handler.git@$BRANCH#egg=anthropic_agent_handler
 echo "y" | pip uninstall ollama_agent_handler && pip install git+https://github.com/ideabosque/ollama_agent_handler.git@$BRANCH#egg=ollama_agent_handler
-echo "y" | pip uninstall mcp_http_client && pip install git+https://github.com/ideabosque/mcp_http_client.git@main#egg=mcp_http_client
-echo "y" | pip uninstall app_core_engine && pip install git+https://github.com/ideabosque/app_core_engine.git@main#egg=app_core_engine
-echo "y" | pip uninstall shopify_app_engine && pip install git+https://github.com/ideabosque/shopify_app_engine.git@main#egg=shopify_app_engine
+echo "y" | pip uninstall mcp_http_client && pip install git+https://github.com/ideabosque/mcp_http_client.git@$BRANCH#egg=mcp_http_client
+echo "y" | pip uninstall app_core_engine && pip install git+https://github.com/ideabosque/app_core_engine.git@$BRANCH#egg=app_core_engine
+echo "y" | pip uninstall shopify_app_engine && pip install git+https://github.com/ideabosque/shopify_app_engine.git@$BRANCH#egg=shopify_app_engine
 
 python3.11 cloudformation_stack.py "$ENV_FILE" silvaengine-microcore-ai-agent
