ccef7306d94d6de23188b9bcda433ee4f02b54a9
Idea Bosque <ideabosque@gmail.com>
2025-10-19 14:38:30 -0700

    Update mcp to support HTTP2.

--- mcp_requirements.sh
+++ mcp_requirements.sh
@@ -9,6 +9,7 @@ fi
 ENV_FILE="$1"
 BRANCH="${2:-main}"
 
+echo "y" | pip uninstall httpx[http2] && pip install httpx[http2]
 echo "y" | pip uninstall silvaengine_resource && pip install git+https://github.com/ideabosque/silvaengine_resouces.git@main#egg=silvaengine_resource
 echo "y" | pip uninstall ai_mcp_daemon_engine && pip install git+ssh://git@github.com/ideabosque/ai_mcp_daemon_engine.git@$BRANCH#egg=ai_mcp_daemon_engine
 
