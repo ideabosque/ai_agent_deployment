7f9bc6bc09cf716670140681ad3c2e1d8419c911
Idea Bosque <ideabosque@gmail.com>
2025-11-10 13:48:37 -0800

    Add ai_rfq_engine.

--- ai_agent_requirements.sh
+++ ai_agent_requirements.sh
@@ -16,7 +16,6 @@ echo "y" | pip uninstall httpx[http2] && pip install httpx[http2]
 echo "y" | pip uninstall shopify_connector && pip install git+https://github.com/ideabosque/shopify_connector.git@main#egg=shopify_connector
 echo "y" | pip uninstall silvaengine_resource && pip install git+https://github.com/ideabosque/silvaengine_resouces.git@main#egg=silvaengine_resource
 echo "y" | pip uninstall ai_agent_core_engine && pip install git+https://github.com/ideabosque/ai_agent_core_engine.git@$BRANCH#egg=ai_agent_core_engine
-echo "y" | pip uninstall ai_marketing_engine && pip install git+https://github.com/ideabosque/ai_marketing_engine.git@$BRANCH#egg=ai_marketing_engine
 echo "y" | pip uninstall ai_coordination_engine && pip install git+https://github.com/ideabosque/ai_coordination_engine.git@$BRANCH#egg=ai_coordination_engine
 echo "y" | pip uninstall ai_agent_handler && pip install git+https://github.com/ideabosque/ai_agent_handler.git@$BRANCH#egg=ai_agent_handler
 echo "y" | pip uninstall openai_agent_handler && pip install git+https://github.com/ideabosque/openai_agent_handler.git@$BRANCH#egg=openai_agent_handler
@@ -24,6 +23,8 @@ echo "y" | pip uninstall gemini_agent_handler && pip install git+https://github.
 echo "y" | pip uninstall anthropic_agent_handler && pip install git+https://github.com/ideabosque/anthropic_agent_handler.git@$BRANCH#egg=anthropic_agent_handler
 echo "y" | pip uninstall ollama_agent_handler && pip install git+https://github.com/ideabosque/ollama_agent_handler.git@$BRANCH#egg=ollama_agent_handler
 echo "y" | pip uninstall travrse_agent_handler && pip install git+https://github.com/ideabosque/travrse_agent_handler.git@$BRANCH#egg=travrse_agent_handler
+echo "y" | pip uninstall ai_marketing_engine && pip install git+https://github.com/ideabosque/ai_marketing_engine.git@$BRANCH#egg=ai_marketing_engine
+echo "y" | pip uninstall ai_rfq_engine && pip install git+https://github.com/ideabosque/ai_rfq_engine.git@$BRANCH#egg=ai_rfq_engine
 echo "y" | pip uninstall mcp_http_client && pip install git+https://github.com/ideabosque/mcp_http_client.git@$BRANCH#egg=mcp_http_client
 echo "y" | pip uninstall app_core_engine && pip install git+https://github.com/ideabosque/app_core_engine.git@$BRANCH#egg=app_core_engine
 echo "y" | pip uninstall shopify_app_engine && pip install git+https://github.com/ideabosque/shopify_app_engine.git@$BRANCH#egg=shopify_app_engine
